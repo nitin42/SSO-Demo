1329d184e4d6675440b160ce332f95010df9b477
Nitin Tulswani <tulswani19@gmail.com>
2016-11-09 00:47:50 +0530

    indexRoute

---
+++ APP-2/routes/home.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const passport = require('passport');
+
+module.exports = function(app) {
+	app.get('/', passport.authenticate('auth0', {}), function(req, res) {
+		res.redirect('/user');
+	});	
+}
